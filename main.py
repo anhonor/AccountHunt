07238f5bdeb8ef88b2e09837b2b824e14c14715d
anhonor <154564843+anhonor@users.noreply.github.com>
2024-02-03 21:53:54 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+import colorama
+from hunt import *
+from hunt.modules.replit import Replit
+
+hunt = Base(
+    input(
+        f'{colorama.Style.BRIGHT}{colorama.Fore.RED}*{colorama.Style.RESET_ALL} Email: '
+    )
+)
+
+functions = [
+   Replit.__check__
+]
+
+for function in functions:
+    function(hunt)
+
+for response in hunt.output:
+    if response[1]['exists']:
+       print(f'{colorama.Style.BRIGHT}{colorama.Fore.LIGHTBLUE_EX}-{colorama.Style.RESET_ALL} {response[1]["module_domain"]} ({response[0]})')
